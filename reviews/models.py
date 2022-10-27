2c7ec677818d146dff509c0d90528f6525d98704
choikeunyoung <ought119@gmail.com>
2022-10-27 16:56:03 +0900

    ADD : 게시글 수정 삭제

--- reviews/models.py
+++ reviews/models.py
@@ -12,8 +12,8 @@ class Article(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     image = ProcessedImageField(upload_to='images/',blank=True, processors=[ResizeToFill(1280, 1024)], format='JPEG', options={'quality':80})
     image_thumbnail = ProcessedImageField(upload_to='images/',blank=True, processors=[Thumbnail(300, 300)], format='JPEG', options={'quality':80})
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
 
 class Comment(models.Model):
