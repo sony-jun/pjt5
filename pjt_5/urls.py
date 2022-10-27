5d67c74b8dc80f7e097e883e2b7fe93502bd8b44
sony-jun <warrise@naver.com>
2022-10-27 15:01:58 +0900

    accounts_app_중간저장

--- pjt_5/urls.py
+++ pjt_5/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include('accounts.url')),
-    path("reviews/", include('reviews.url')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("reviews/", include("reviews.urls")),
 ]
