581c268050288899ee3daa275daacb1477db8600
sony-jun <warrise@naver.com>
2022-10-28 11:25:30 +0900

    update

--- pjt_5/settings.py
+++ pjt_5/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = BASE_DIR / "images"
